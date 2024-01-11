da8c5acc67051a09b10a69e876566edce9fbee18
spencera0024 <97623604+spencera0024@users.noreply.github.com>
2024-01-11 10:42:52 -0600

    String interpolation

--- AndersonPlayground.playground/Contents.swift
+++ AndersonPlayground.playground/Contents.swift
@@ -2,3 +2,11 @@ import UIKit
 
 var greeting = "Hello, playground"
 print("Hello, World!")
+
+var pl = "Swift";
+print("My favorite pl is \(pl)");
+print("My favorite pl is " + pl);
+
+
+var salary = 10.5;
+print("My salary is \(salary)");
