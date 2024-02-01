60460878a85a93b073221a63485a0aca50d86b2d
spencera0024 <97623604+spencera0024@users.noreply.github.com>
2024-02-01 09:58:29 -0600

    Created connections

--- DiscountApp/DiscountApp/ViewController.swift
+++ DiscountApp/DiscountApp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var amountOL: UITextField!
+    
+    @IBOutlet weak var discountOL: UITextField!
+    
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    @IBOutlet weak var discountLabel: UILabel!
+    
+    @IBOutlet weak var finalAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcBtnClicked(_ sender: Any) {
+    }
 }
 
