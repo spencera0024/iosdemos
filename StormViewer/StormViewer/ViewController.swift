de74158ca89ff9c47d7de77c2cd2582917fe3647
spencera0024 <97623604+spencera0024@users.noreply.github.com>
2024-02-19 20:16:28 -0600

    Tried doing a tutorial but the code doesn't work

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Spencer Anderson on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+            print(pictures)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
+
